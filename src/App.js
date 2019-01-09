545d9b5d00c30f3f41b4ecc12f1be9f497509bbd
Kylee Tilley <kyleetilley@gmail.com>
2019-01-08 23:38:35 -0600

    Implement basic counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import "./App.css";
 
+const Button = styled.button`
+  font-size: 2em;
+  width: 200px;
+`;
+
+const Counter = styled.div`
+  font-size: 2em;
+`;
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  increment = e => {
+    e.preventDefault();
+    const counter = this.state.counter + 1;
+    this.setState({ counter });
+  };
+
+  decrement = e => {
+    e.preventDefault();
+    const counter = this.state.counter - 1;
+    this.setState({ counter });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p />
+          <Button onClick={this.increment}>+</Button>
+
+          <Counter>{this.state.counter}</Counter>
+
+          <Button onClick={this.decrement}>-</Button>
         </header>
       </div>
     );
